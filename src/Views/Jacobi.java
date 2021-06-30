5497a5c22b125ab6879ffd6223cc0e02b3bcb0ae
NThomska <86089120+NThomska@users.noreply.github.com>
2021-06-29 18:24:10 -0600

    bug fixes

--- src/Views/Jacobi.java
+++ src/Views/Jacobi.java
@@ -417,10 +417,14 @@ public class Jacobi extends javax.swing.JFrame {
 
     private void btnPrepararActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrepararActionPerformed
     
+        if (txtOrden.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(rootPane, "Debe ingresar el tamaño de la matriz", "Orden de la matriz nula", JOptionPane.WARNING_MESSAGE);
+        }else{
         startTable();
         rango = Integer.parseInt(txtOrden.getText());
         btnCalcular.setEnabled(true);
         PrepararMatrices();
+        }
         
     }//GEN-LAST:event_btnPrepararActionPerformed
 
